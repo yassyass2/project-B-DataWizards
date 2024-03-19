087ad7bc7ace28ede5b0e3e9750738638ac39b64
yassyass2 <yassinabde@outlook.com>
2024-03-19 22:20:33 +0100

    Reservation.cs uitgebreid
    functie toegevoegd voor printen details, en funcites voor aanpassen datum/hoeveelheid mensen

--- Reservation.cs
+++ Reservation.cs
@@ -1,5 +1,11 @@
 class Reservation
 {
     private int _people;
+    private string _date;
+
+    public void Info() => Console.WriteLine($"Reservation on {_date} for {_people} people");
+
+    public void ChangeDate(string newdate) => _date = newdate;
+    public void ChangePeople(int AmountOfPeople) => _people = AmountOfPeople;
 
 }
