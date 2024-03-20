72baa6fb0dc6ea57371e1e8d76ff383cc828db65
Lucas-932 <lucas@familie-reedijk.nl>
2024-03-20 14:37:21 +0100

    Program.cs (choiceMenu)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿class Program
+﻿using System.Linq.Expressions;
+using System.Xml.Serialization;
+
+class Program
 {
-    static void Main()
+    public static void Main()
     {
         Console.WriteLine(@"
          /$$   /$$ /$$     /$$       /$$$$$$$  /$$        /$$$$$$   /$$$$$$  /$$$$$$$$
@@ -12,5 +15,19 @@
         | $$ \  $$    | $$          | $$      | $$$$$$$$| $$  | $$|  $$$$$$/| $$$$$$$$
         |__/  \__/    |__/          |__/      |________/|__/  |__/ \______/ |________/
             ");
+        while (true)
+        {
+            Console.WriteLine("Welcome to NY Place");
+            Console.WriteLine("Do you want to reserve or see the menu?");
+            string? choice = Console.ReadLine();
+            if (choice == "reserve")
+            {
+                Reservation.ReservationDetails()
+            }
+            else if (choice == "menu")
+            {
+                //Reservation.Menu()
+            }
+        }
     }
 }
