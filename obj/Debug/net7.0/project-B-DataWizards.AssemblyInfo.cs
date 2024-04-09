0707cb66ecee55ecb06c17c7a8798e0aef0718f4
yassyass2 <yassinabde@outlook.com>
2024-04-09 11:35:08 +0200

    bestanden aan gitignore toegevoegd

--- obj/Debug/net7.0/project-B-DataWizards.AssemblyInfo.cs
+++ obj/Debug/net7.0/project-B-DataWizards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-B-DataWizards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba5ce6c2fe32f562c9ec76675bbf34f7885dfb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966508dd3fa36c5f586ca5d7296be968197538d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-B-DataWizards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-B-DataWizards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
