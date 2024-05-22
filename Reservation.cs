0601682b57bb054a71f89fd3a77774de3fb26ff6
DonovanFeller <donovanfellerr@gmail.com>
2024-05-22 14:16:47 +0200

    Reserveren en hierbij de minuut kiezen is verbeterd

--- Reservation.cs
+++ Reservation.cs
@@ -118,7 +118,7 @@ class Reservation
 
         hour = ReservationSystem.GetValidDate("Vul een tijd in (19-23): ", 19, 23);
 
-        minute = ReservationSystem.GetValidMinute("Vul een minuut-optie in (0 - 15 - 30 - 45): ");
+        minute = ReservationSystem.GetValidMinute($"Vul een van de mogelijke tijden in {hour}:(00 - 15 - 30 - 45): "); // "Vul een minuut-optie in (0 - 15 - 30 - 45): "
         DateTime date = new DateTime(year, month, day, hour, minute, 0);
 
 
